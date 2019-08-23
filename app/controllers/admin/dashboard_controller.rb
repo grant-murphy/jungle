70de70de09678384e038e04e17c523c75b66ef6e
grant-murphy <murphyg.gm@gmail.com>
2019-08-23 03:25:27 +0000

    Implemented HTTP basic auth to secure admin/products on dashboard_controller, products_controller, and application_controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with :name =>  ENV["HTTP_BASIC_USER"], 
+  :password => ENV["HTTP_BASIC_PASSWORD"], except: [:index]
+
   def show
   end
 end
