0acdb7f4662f2d1bafa3110f9e27fc6a01e1daf3
grant-murphy <murphyg.gm@gmail.com>
2019-08-26 23:57:59 +0000

    Refactored Code, switched validation field to being tested to nil

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,28 +2,31 @@ require 'rails_helper'
 
 RSpec.describe Product, :type => :model do
   describe 'Validations' do
-    
-    it 'will be valid  if all 4 fields are present' do
+    before(:each) do
       @category = Category.new(id: 1, name: 'Apparel')
       @product = Product.new(name: 'Shirt', quantity: 1, price: 19.99, category: @category)
+    end
+
+    it 'will be valid  if all 4 fields are present' do
+      @product.save
       expect(@product).to be_valid
       end
 
     it 'will not be valid if price is not present' do
-      @category = Category.new(id: 1, name: 'Apparel')
-      @product = Product.new(name: 'Shirt', quantity: 1, category: @category)
+      @product.price_cents = nil
+      @product.save
       expect(@product).to_not be_valid
       end
       
       it 'will not be valid if quantity is not present' do
-      @category = Category.new(id: 1, name: 'Apparel')
-      @product = Product.new(name: 'Shirt', price: 19.99, category: @category)
+      @product.quantity = nil
+      @product.save
       expect(@product).to_not be_valid
       end
       
       it 'will not be valid if category is not present' do
-      # @category = Category.new(id: 1, name: 'Apparel')
-      @product = Product.new(name: 'Shirt', quantity: 1, price: 19.99, category: @category)
+      @product.category = nil
+      @product.save
       expect(@product).to_not be_valid
     end
   end
