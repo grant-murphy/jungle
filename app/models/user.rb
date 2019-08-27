571b8517804a33e80da99e4aa6edcb8474261f2c
grant-murphy <murphyg.gm@gmail.com>
2019-08-27 23:39:36 +0000

    Added New Authentication method which returns an instance of the user or nil (email/password are equal/white space/upper and lower case

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,21 @@
 class User < ActiveRecord::Base
-
+  
   has_secure_password 
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
 
+
+def self.authenticate_with_credentials(email, password)
+  @user = User.find_by_email(email.downcase.split)
+  if @user && @user.authenticate(password)
+    @user
+    # success logic, log them in
+  else
+    nil
+    # failure, render login form
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,19 +7,75 @@ RSpec.describe User, :type => :model do
       @user = User.new(
         name: 'Grant', 
         email: 'test@test.com', 
-        password: 'grantr', 
-        password_confirmation: 'grantr')
+        password: 'grantm', 
+        password_confirmation: 'grantm')
     end
       
-      it 'will be valid' do
+      it 'will be valid if all sections are filled out' do
         @user.save
         expect(@user).to be_valid
       end
       
       it 'will not be valid if email does not exist' do
-      @user.email= nil
+      @user.email = nil
       @user.save
       expect(@user).to_not be_valid
       end
-    end
+
+      it 'will not be valid if name does not exist' do
+        @user.name = nil
+        @user.save
+        expect(@user).to_not be_valid
+        end
+
+      it 'will not be valid if password does not exist' do
+        @user.password = nil
+        @user.save
+        expect(@user).to_not be_valid
+        end
+
+      it 'will not be valid if password_confirmation does not exist' do
+        @user.password_confirmation = nil
+        @user.save
+        expect(@user).to_not be_valid
+        end
+
+      it 'will not be valid if password and password_confirmation do not match' do
+        @user.password_confirmation = "grantmm"
+        @user.save
+        expect(@user).to_not be_valid
+        end
+
+      it 'will not be valid if password is shorter than 6 characters' do
+        @user.password = "grant"
+        @user.password_confirmation = "grant"
+        @user.save
+        expect(@user).to_not be_valid
+        end
+      end
+
+      describe '.authenticate_with_credentials' do
+        before(:each) do
+          @user = User.new(
+          name: 'Grant', 
+          email: 'test@test.com', 
+          password: 'grantm', 
+          password_confirmation: 'grantm')
+        end
+
+        it 'will be valid if email and password are equal' do
+          @user.save
+          expect(User.authenticate_with_credentials(@user.email, @user.password)).not_to be nil
+        end
+
+        it 'will be valid when email has white-space' do
+          @user.save
+          expect(User.authenticate_with_credentials(' test@test.com ', 'grantm')).not_to be nil
+        end
+
+        it 'will be valid when email has upper and lower case letters combined' do
+          @user.save
+          expect(User.authenticate_with_credentials(' TeSt@test.com ', 'grantm')).not_to be nil
+        end
+  end
 end 
